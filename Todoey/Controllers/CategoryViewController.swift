144e058f57927f5f3f901737720c7c1325583088
analitik9 <analitik9@gmail.com>
2018-10-30 15:55:02 +0300

    final of project

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 import RealmSwift
+import  ChameleonFramework
 
-class CategoryViewController: UITableViewController {
+
+class CategoryViewController: SwipeTableViewController{
+   
  
-    var categories: Results<Category>!
-    let realm = try? Realm()
+    var categories: Results<Category>?
+    let realm = try! Realm()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategory()
+        tableView.separatorStyle = .none
+   
     }
 
     
@@ -28,11 +33,10 @@ class CategoryViewController: UITableViewController {
             if let name = textField.text{
                 let newCategory = Category()
                 newCategory.name = name
-                //   self.categoryArray.append(newCategory)
+                newCategory.color = UIColor.randomFlat()?.hexValue()
                 self.save(category: newCategory)
-                
             }
-            
+            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -44,6 +48,7 @@ class CategoryViewController: UITableViewController {
         
     }
     
+ 
      //MARK: - TalbeView Delegate Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return  categories?.count ?? 1
@@ -53,6 +58,8 @@ class CategoryViewController: UITableViewController {
     override  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "goToItems", sender: self)
     }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! ToDoListViewController
         if let indexPath = tableView.indexPathForSelectedRow{
@@ -62,15 +69,21 @@ class CategoryViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let catName = categories?[indexPath.row].name ?? "no category add yet"
-        cell.textLabel?.text = catName
+   
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "no category add yet"
+       
+        guard let categoryColor = categories?[indexPath.row].color else { fatalError()}
+        
+        cell.backgroundColor = UIColor(hexString: categoryColor)
+        cell.textLabel?.textColor = ContrastColorOf(backgroundColor: UIColor(hexString: categoryColor), returnFlat: true)
+
         return cell
     }
     
     //MARK: - Data Manipulation Methods
     func loadCategory(){
-         categories = realm?.objects(Category.self)
+         categories = realm.objects(Category.self)
         
        tableView.reloadData()
     }
@@ -78,13 +91,31 @@ class CategoryViewController: UITableViewController {
     func save(category: Category){
         
         do{
-            try realm!.write {
-                realm!.add(category)
+            try realm.write {
+                realm.add(category)
             }
         }catch{
             print("Error saving context \(error)")
         }
-       tableView.reloadData()
+      
     }
+    
+    //MARK: -  Delete Data from swipe
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row]{
+            do{
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            }catch{
+                print("Error deleting category \(error)")
+            }
+        }
+    }
+    }
+    
 
-}
+
+
+
+
--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: SwipeTableViewController {
 
+    @IBOutlet weak var searhBar: UISearchBar!
+    
     var todoItems: Results<Item>?
     var selectedCategory: Category?{
         didSet {
-           //loadItems()
+           loadItems()
         }
     }
     let realm = try! Realm()
@@ -22,8 +25,34 @@ class ToDoListViewController: UITableViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
       // print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-     
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = selectedCategory?.name
+        guard let hexColour = selectedCategory?.color else{fatalError()}
+        updateNavBar(withHexCode: hexColour)
+ 
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+       
+       updateNavBar(withHexCode: "1D9BF6")
+    }
+    
+    
+    
+    func updateNavBar(withHexCode colourHexCode: String){
+        
+        guard let tintColor = UIColor(hexString: colourHexCode) else {fatalError()}
+        guard let navBar = navigationController?.navigationBar else {fatalError("navBar not existing")}
+        navBar.barTintColor = tintColor
+        navBar.tintColor = ContrastColorOf(backgroundColor: tintColor, returnFlat: true)
+        navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: ContrastColorOf(backgroundColor: tintColor, returnFlat: true)]
+        searhBar.tintColor = tintColor
     }
     
     //MARK - Tableview DataSource Methods
@@ -33,13 +62,17 @@ class ToDoListViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let title =  todoItems?[indexPath.row].title ?? "no item added yet"
-        let done = todoItems?[indexPath.row].done ?? false
-        cell.textLabel?.text = title
-        
-        cell.accessoryType = done == true ? .checkmark : .none
-
+        // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        if  let item =  todoItems?[indexPath.row]{
+            if let color = UIColor(hexString:selectedCategory!.color).darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)){
+                
+                cell.accessoryType = item.done == true ? .checkmark : .none
+                cell.textLabel?.text = item.title
+                cell.backgroundColor = color
+                cell.textLabel?.textColor = ContrastColorOf(backgroundColor: color, returnFlat: true)
+            }
+        }
         return cell
     }
     //MARK -  TableView Delegate Methods
@@ -55,13 +88,7 @@ class ToDoListViewController: UITableViewController {
         }
         
         tableView.reloadData()
-        
-        //  itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        //      saveItems()
-        
-        //   tableView.deselectRow(at: indexPath, animated: true)
-     
+       
     }
     
     //MARK  - Add new items
@@ -92,15 +119,27 @@ class ToDoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        self.tableView.reloadData()
     }
 
     //MARK  - Model Manipulation Methods
 
 
-
     func loadItems(){
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
+    }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = self.todoItems?[indexPath.row]{
+            do{
+                try self.realm.write {
+                    self.realm.delete(item)
+                }
+            }catch{
+                print("Error deleting category \(error)")
+            }
+        }
     }
 
 }
